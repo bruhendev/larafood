923abac1653abc2817d4128f6b151948e0cf3af2
Bruno Henrique <brunohenrique242@gmail.com>
2023-08-23 14:38:27 -0300

    create index plan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PlanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
+Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
+Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
+Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
